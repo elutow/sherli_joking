d980c43b90a5bb4db3ce195736c1e5d3b2efd212
Elton <eloston@uw.edu>
2019-05-15 21:43:49 +0000

    Update more devel-related config

--- devel/process.sh
+++ devel/process.sh
@@ -3,5 +3,6 @@
 set -eux
 
 cd $(dirname $(dirname $(readlink -f $0)))
-yapf -ipr sherlibot slowbro sherli_server.py tests
-pylint sherlibot slowbro sherli_server.py tests
+yapf -ipr sherlibot slowbro sherlibot_server.py tests
+#mypy --ignore-missing-imports sherlibot slowbro sherlibot_server.py tests
+pylint sherlibot slowbro sherlibot_server.py.py tests
--- devel/test.sh
+++ devel/test.sh
@@ -3,4 +3,4 @@
 set -eux
 
 cd $(dirname $(dirname $(readlink -f $0)))
-pytest --cov= --cov-report term-missing
+pytest
