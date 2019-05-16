eb1f8ceee1aed8a222685c1937c28d7fafd26768
Elton <eloston@uw.edu>
2019-05-16 04:16:28 +0000

    Tweak pylint output

--- devel/process.sh
+++ devel/process.sh
@@ -5,4 +5,4 @@ set -eux
 cd $(dirname $(dirname $(readlink -f $0)))
 yapf -ipr sherlibot slowbro sherlibot_server.py tests
 #mypy --ignore-missing-imports sherlibot slowbro sherlibot_server.py tests
-pylint sherlibot slowbro sherlibot_server.py.py tests
+pylint sherlibot sherlibot_server.py.py tests
